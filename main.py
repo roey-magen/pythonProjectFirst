f8c95bc021c3c3d8ddb33a091adb63c159cd4bd7
roey-magen <57194211+roey-magen@users.noreply.github.com>
2021-09-13 15:09:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print('First roey project A2')  # Press Strg+F8 to toggle the breakpoint.
+    print('First roey project A3')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
